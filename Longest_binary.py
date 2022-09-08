352cc23b09e32b1812b5472024b6c12aed56b89d
birabattlasanathkumar <birabattlasanathkumar@gmail.com>
2022-09-08 13:58:35 +0530

    Lonest binary

---
+++ Longest_binary.py
@@ -0,0 +1,13 @@
+def maxConsecutive1(input):
+    input = bin(2147483647)
+
+    input = input[2:]
+    print(input)
+
+    print(max(map(len, input.split('1'))))
+
+
+if __name__ == '__main__':
+    maxConsecutive1(input)
+
+
